43a262382cb7414feb5f0eaf9a8d3290848eea63
Daniel Hammond <viztheman@protonmail.com>
2023-02-05 21:14:15 -0500

    Adding bulk recipes API call, API key, and default DB path/name.

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const {PORT} = process.env;
 
 const express = require('express');
 const path = require('path');
+const apiKey = require('./lib/apikey');
 
 const app = express();
 app.set('view engine', 'pug');
 app.use('/css', express.static(path.join(__dirname, 'public/css')));
 app.use('/js', express.static(path.join(__dirname, 'public/js')));
 app.use('/', require('./lib/routers/web/root'));
-app.use('/api', require('./lib/routers/api/recipes'));
+app.use('/api', apiKey(), require('./lib/routers/api/recipes'));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
